1bf35ac6bb41b03724c5ff2f94185d74ec49416f
Adil Rizwan <adilrizwan2001@gmail.com>
2023-10-04 16:08:56 +0500

    Login & Register

--- WebApp/backend/db/sqlConfig.js
+++ WebApp/backend/db/sqlConfig.js
@@ -22,7 +22,7 @@ const pool = mssql.connect(config);
 const connectMSSQL = async () => {
   try {
     if (await pool !== undefined) {
-      console.log("Connected to SQL Server".underline.magenta);
+      console.log("Connected to Azure SQL".underline.magenta);
     }
   } catch (error) {
     console.log(error);
