a7eb43ded43af2a78d4085c73492246dc8cb9227
Adil Rizwan <adilrizwan2001@gmail.com>
2023-12-18 20:18:45 +0500

    minor fixes

--- WebApp/backend/controllers/loginController.js
+++ WebApp/backend/controllers/loginController.js
@@ -7,50 +7,47 @@ exports.loginAuth = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!validator.isEmail(email) || !password) {
-      res.status(400).json({ message: "Enter all required fields." });
-    } else {
-      const pass = await loginOps.loginOps(email);
-      if (pass === 0) {
-        res.status(401).send("Incorrect Email");
-        return;
+      return res.status(400).json({ message: "Enter all required fields." });
+    }
+
+    const pass = await loginOps.loginOps(email);
+    if (pass === 0) {
+      return res.status(401).json({ message: "Invalid Credentials." });
+    }
+
+    if (await bcrypt.compare(password, pass.Password)) {
+      const details = await loginOps.getDetails(email, pass.UserRole);
+      let jwToken;
+      if (
+        pass.UserRole.toUpperCase() === "CAROWNER" ||
+        pass.UserRole.toUpperCase() === "ADMIN"
+      ) {
+        jwToken = generateToken(
+          pass.UserRole.toUpperCase(),
+          details.ID,
+          details.FirstName
+        );
       } else {
-        if (await bcrypt.compare(password, pass.Password)) {
-          const details = await loginOps.getDetails(email, pass.UserRole);
-          var jwToken;
-          if (
-            pass.UserRole.toUpperCase() === "CAROWNER" ||
-            pass.UserRole.toUpperCase() === "ADMIN"
-          ) {
-            jwToken = generateToken(
-              pass.UserRole.toUpperCase(),
-              details.ID,
-              details.FirstName
-            );
-          } else {
-            jwToken = generateToken(
-              pass.UserRole.toUpperCase(),
-              details.ID,
-              details.Name
-            );
-          }
-          res.send({
-            token: jwToken,
-            role: pass.UserRole,
-            Details: details
-          })
-          return;
-        } else {
-          res.status(401).send("Incorrect password.");
-          return;
-        }
+        jwToken = generateToken(
+          pass.UserRole.toUpperCase(),
+          details.ID,
+          details.Name
+        );
       }
+      return res.send({
+        token: jwToken,
+        role: pass.UserRole,
+        Details: details,
+      });
+    } else {
+      return res.status(401).json({ message: "Invalid Credentials." });
     }
   } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: error });
-    return;
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 const generateToken = (role, id, name) => {
   return jwt.sign({ role, id, name }, process.env.JWT_SECRET, {
     expiresIn: "10000s",
