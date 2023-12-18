7881d9505785f64bb75f36698b432d24bd7029fd
TalhaAhmed2001 <talhacool01@gmail.com>
2023-12-18 19:29:48 +0500

    controller defined

--- MobileApp/backend/controllers/parkingSpaceController.js
+++ MobileApp/backend/controllers/parkingSpaceController.js
@@ -1,1 +1,88 @@
-meow
+const { fb } = require("../db/firebaseConfig")
+const fieldValue = require('firebase-admin').firestore.FieldValue;
+
+const getEmptySpaces = async (req, res) => {
+
+    const lot = "lot_" + req.params.lotid
+    const zone = "zone_" + req.params.zoneid
+
+    try {
+        const doc = await fb.collection(lot).doc(zone).get();
+
+        if (doc.exists) {
+            // console.log(doc)
+            res.status(200).json(doc._fieldsProto.empty.integerValue)
+        }
+        else {
+            // console.log("data not here")
+            res.status(400).json({ error: "No doc found" })
+        }
+    }
+    catch (err) {
+        // console.log("errpr")
+        res.status(500).json({ error: `Error executing query: ${err}` })
+    }
+}
+
+const updateEmptySpaces = async (req, res) => {
+
+    const lot = "lot_" + req.params.lotid
+    const zone = "zone_" + req.params.zoneid
+    const empty = req.body.empty
+
+    try{
+        const doc = fb.collection(lot).doc(zone);
+
+        doc.update({
+          empty: empty
+        });
+        res.status(200).json()
+    }
+    catch(err){
+        res.status(500).json({ error: `Error executing query: ${err}` })
+    }
+}
+
+const incEmptySpaces = async (req, res) => {
+
+    const lot = "lot_" + req.params.lotid
+    const zone = "zone_" + req.params.zoneid
+
+    try{
+        const doc = fb.collection(lot).doc(zone);
+
+        await doc.update({
+          empty: fieldValue.increment(1)
+        });
+        res.status(200).json()
+    }
+    catch(err){
+        res.status(500).json({ error: `Error executing query: ${err}` })
+    }
+}
+
+const decEmptySpaces = async (req, res) => {
+
+    const lot = "lot_" + req.params.lotid
+    const zone = "zone_" + req.params.zoneid
+
+    try{
+        const doc = fb.collection(lot).doc(zone);
+        
+        await doc.update({
+          empty: fieldValue.increment(-1)
+        })
+
+        return res.status(200).json()
+    }
+    catch(err){
+        res.status(500).json({ error: `Error executing query: ${err}` })
+    }
+}
+
+module.exports = {
+    getEmptySpaces,
+    updateEmptySpaces,
+    incEmptySpaces,
+    decEmptySpaces
+}
--- MobileApp/backend/server.js
+++ MobileApp/backend/server.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
+const cors = require('cors');
 const {detectSpaces} = require('./cv/parkingController')
+const parkingSpaceRouter = require("./routes/parkingSpaceRouter")
 require("colors");
 
 const express = require("express");
-const { connectFirebase, fb } = require("./db/firebaseConfig");
+const { connectFirebase } = require("./db/firebaseConfig");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-const PORT = process.env.PORT || 8000;
-
+app.use("/", parkingSpaceRouter)
+const PORT = 5000;
+// process.env.PORT
 connectFirebase();
-detectSpaces();
+// detectSpaces();
 
 app.listen(PORT, () => {
   console.log(`\nMobile App Server is listening on ${PORT}`.underline.green);
