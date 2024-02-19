76e3b1d47538a1ac5bb8c11e81a13af6b9f3880b
Adil Rizwan <adilrizwan2001@gmail.com>
2024-02-20 01:06:02 +0500

    car owner backend complete

--- WebApp/backend/routes/carOwnerRoutes.js
+++ WebApp/backend/routes/carOwnerRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const register = require("../controllers/registerController");
 const carOwnerController = require("../controllers/carOwnerController");
 const auth = require("../middleware/auth");
 
+router.post("/register" , register.registerAuth)
 
-router.get("/", auth , carOwnerController.dashboard)
+router.get("/", auth , carOwnerController.viewVehicles)
 router.get("/profile", auth, carOwnerController.getProfile)
 router.put("/profile", auth , carOwnerController.updateProfile)
-router.post("/add", auth , carOwnerController.addCar)
-router.delete("/delete", auth , carOwnerController.deleteCar)
+router.post("/", auth , carOwnerController.addCar)
+router.post("/support", auth , carOwnerController.support)
+router.delete("/:regno", auth , carOwnerController.deleteCar)
 
 module.exports = router;
