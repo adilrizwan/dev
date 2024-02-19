88089f849b0ed612c11d7378abac2228a453a8dc
Adil Rizwan <adilrizwan2001@gmail.com>
2024-02-20 01:12:04 +0500

    lot owner skeleton

---
+++ WebApp/frontend/src/scenes/dashboard/LotOwner/LotDashboard.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { useTheme, Box, ThemeProvider, Container, Grid } from '@mui/material';
+import SideBar from './LotDrawer';
+
+export default function LotDashboard() {
+  const theme = useTheme();
+  // const [currentComponent, setCurrentComponent] = React.useState(<JobsApplied />);
+
+  // const handleProfileClick = () => {
+  //   setCurrentComponent(<ProfileApplicant />);
+  // };
+  // const handleDashboardClick = () => {
+  //   setCurrentComponent(<JobsApplied />);
+  // };
+  // const handleSearchClick = () => {
+  //   setCurrentComponent(<FindJobs />);
+  // };
+  // const handleCVClick = () => {
+  //   setCurrentComponent(<DownloadCV />);
+  // };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <Grid>
+          <SideBar
+          // onDashboardClick={handleDashboardClick}
+          // onUpdateProfileClick={handleProfileClick}
+          // onSearchClick={handleSearchClick}
+          // onCVClick={handleCVClick}
+          />
+        </Grid>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            overflow: 'auto',
+          }}
+        >
+          <Container sx={{ mt: 3 }}>
+
+            <Grid>
+              {/* {currentComponent} */}
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
