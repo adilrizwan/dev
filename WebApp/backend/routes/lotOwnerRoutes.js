02ce8d6556ae1eb3a1741e30bbc0880188f3c015
Adil Rizwan <adilrizwan2001@gmail.com>
2024-02-20 01:06:30 +0500

    minor fixes

--- WebApp/backend/routes/lotOwnerRoutes.js
+++ WebApp/backend/routes/lotOwnerRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const lotOwnerController = require("../controllers/lotOwnerController");
+const register = require("../controllers/registerController");
 const auth = require("../middleware/auth");
 
+router.post("/register" , register.registerAuth)
 
 router.get("/", auth , lotOwnerController.dashboard)
 router.get("/profile", auth, lotOwnerController.getProfile)
